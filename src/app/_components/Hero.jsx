474db4572d1489b97ed996d1772342e50f715a5d
Surya-0804 <surya.abothula@gmail.com>
2024-10-03 15:21:09 +0530

    minor landing page changes

--- src/app/_components/Hero.jsx
+++ src/app/_components/Hero.jsx
@@ -5,16 +5,13 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <section className="bg-gray-50 flex items-center flex-col pt-0 mt-0">
-      {" "}
       {/* Remove margin-top */}
       <div className="flex flex-col overflow-hidden mt-0">
-        {" "}
         {/* Remove margin-top */}
         <ContainerScroll
           titleComponent={
             <>
               <h1 className="text-4xl mb-5 font-semibold text-black dark:text-white">
-                {" "}
                 {/* Adjust margin-bottom */}
                 AI-powered to manage your expenses
                 <br />
